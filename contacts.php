b2affa7e99dd6fd812099e7fafb29d2d47258d7c
saritasakkad <32385715+saritasakkad@users.noreply.github.com>
2017-10-06 16:40:53 +0200

    new desing
    les fichier modifier

---
+++ contacts.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<title>cv akkad</title>
+
+        <meta name="viewport" content="width=device-width" />
+        <link rel="stylesheet" href="./asset/css/contacts.css">
+        <link rel="stylesheet" href="jquery-3.2.1.js" />
+				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
+
+	</head>
+	<body>
+
+<!-- navigateur -->
+
+
+			<div class="overlay-navigation">
+			<nav role="navigation">
+				<ul>
+					<li><a href="index.php" data-content="L'esencielle" title="HOME">Home</a></li>
+					<li><a href="portfolio.php" data-content="Qualiter 1" title="Skills">Skills</a></li>
+					<li><a href="contacts.php" data-content="Appel moi" title="About">About</a></li>
+					<li><a href="#" data-content="#">Works</a></li>
+					<li><a href="#" data-content="#">Contact</a></li>
+				</ul>
+			</nav>
+			</div>
+
+<!-- menu BG -->
+
+			<div class="open-overlay">
+				<span class="bar-top"></span>
+				<span class="bar-middle"></span>
+				<span class="bar-bottom"></span>
+			</div>
+
+
+
+<!-- content-section3 -->
+
+        <div class="content-section3">
+
+					<h1>C Section</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget aliquam massa.</p></br>
+					<p>Donec feugiat, tortor ac feugiat auctor, purus augue rhoncus neque, at tristique tortor urna at velit.</p></br>
+					<p>Nullam in maximus quam. Suspendisse aliquam enim nibh, ac scelerisque justo sollicitudin eget. In sed lacinia quam, a viverra diam.</p>
+
+					<!-- formuler -->
+
+					<div class="formulair">
+
+						  <form>
+						  <ul class="form-style-1">
+						      <li><label>Full Name <span class="required">*</span></label><input type="text" name="field1" class="field-divided" placeholder="First" />&nbsp;<input type="text" name="field2" class="field-divided" placeholder="Last" /></li>
+						      <li>
+						          <label>Email <span class="required">*</span></label>
+						          <input type="email" name="field3" class="field-long" />
+						      </li>
+						      <li>
+						          <label>Subject</label>
+						          <select name="field4" class="field-select">
+						          <option value="Advertise">Advertise</option>
+						          <option value="Partnership">Partnership</option>
+						          <option value="General Question">General</option>
+						          </select>
+						      </li>
+						      <li>
+						          <label>Your Message <span class="required">*</span></label>
+						          <textarea name="field5" id="field5" class="field-long field-textarea"></textarea>
+						      </li>
+						      <li>
+						          <input type="submit" value="Submit" />
+						      </li>
+						  </ul>
+						  </form>
+
+  					</div>
+
+		</div>
+
+
+        
+
+<!-- footer -->
+
+<footer class="footer" ><a href="">ckmjnfmdkqngepM?VMS</a></footer>
+
+        <script src="jquery-3.2.1.js"></script>
+
+
+
+				<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+			<script src='https://cdnjs.cloudflare.com/ajax/libs/velocity/1.2.3/velocity.min.js'></script>
+			<script src='https://cdnjs.cloudflare.com/ajax/libs/velocity/1.2.3/velocity.ui.min.js'></script>
+
+			<script  src="asset/js/index.js"></script>
+
+
+
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<title>cv akkad</title>
+
+        <meta name="viewport" content="width=device-width" />
+        <link rel="stylesheet" href="./asset/css/style.css">
+        <link rel="stylesheet" href="jquery-3.2.1.js" />
+
+
+
+
+
+	</head>
+	<body>
+
+<!-- navigateur -->
+
+
+			<div class="overlay-navigation">
+			<nav role="navigation">
+				<ul>
+					<li><a href="index.php" data-content="L'esencielle" title="HOME">Home</a></li>
+					<li><a href="portfolio.php" data-content="Qualiter 1" title="Skills">Skills</a></li>
+					<li><a href="contacts.php" data-content="Appel moi" title="About">About</a></li>
+					<li><a href="#" data-content="#">Works</a></li>
+					<li><a href="#" data-content="#">Contact</a></li>
+				</ul>
+			</nav>
+			</div>
+
+<!-- menu BG -->
+
+			<div class="open-overlay">
+				<span class="bar-top"></span>
+				<span class="bar-middle"></span>
+				<span class="bar-bottom"></span>
+			</div>
+
+
+
+
+<!-- content-section -->
+
+
+		<div class="content-section">
+
+			<svg viewBox="0 0 960 300">
+	<symbol id="s-text">
+		<text text-anchor="middle" x="50%" y="80%">CV AKKAD</text>
+	</symbol>
+
+	<g class = "g-ants">
+		<use xlink:href="#s-text" class="text-copy"></use>
+		<use xlink:href="#s-text" class="text-copy"></use>
+		<use xlink:href="#s-text" class="text-copy"></use>
+		<use xlink:href="#s-text" class="text-copy"></use>
+		<use xlink:href="#s-text" class="text-copy"></use>
+	</g>
+</svg>
+
+<div class="dreams">
+
+
+<img src="asset/images/dreams.png" alt="pdf">
+
+
+</div>
+
+</div>
+
+
+				<div class="brid">
+
+				<h1>Bienvenu</h1>
+				<p>Tu veux me décoder.</p></br>
+				<p>Apprend à me découvrir.</p></br>
+				<p>Moi mon dada chez le web designer</br> & le web development & mobile.</p>
+
+				</div>
+
+
+
+<!-- footer -->
+
+<footer class="footer" ><a href="http://akkadwebdesigner.16mb.com/" target="_blank">AKKAD webdesig</a></footer>
+
+        <script src="jquery-3.2.1.js"></script>
+
+
+
+				<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+			<script src='https://cdnjs.cloudflare.com/ajax/libs/velocity/1.2.3/velocity.min.js'></script>
+			<script src='https://cdnjs.cloudflare.com/ajax/libs/velocity/1.2.3/velocity.ui.min.js'></script>
+
+			<script  src="asset/js/index.js"></script>
+
+			<script type="text/javascript" script-name="condiment" src="http://use.edgefonts.net/condiment.js"></script>
+
+
+
+	</body>
+</html>
